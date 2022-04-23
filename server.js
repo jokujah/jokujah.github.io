376b06a5c8a34232702e58956b4f6e0e0c210d2b
okuja <ajokuja@gmail.com>
2022-04-23 14:39:46 +0300

    Okuja | Feature : Added express server to host to heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+const express = require('express');
+const path = require('path');
+const app = express();
+//app.use(requireHTTPS);
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/egpReportsUI'));
+
+app.get('/*', function(req,res) {    
+    res.sendFile(path.join(__dirname+'/dist/egpReportsUI/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
