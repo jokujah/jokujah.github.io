cb20407053a217a4d3ebf5c32a46e8f176c19946
okuja <ajokuja@gmail.com>
2022-05-09 19:51:41 +0300

    Chore: Due deligence visualisations

--- src/app/pages/evaluation-category-page/due-deligence-report/due-deligence-visuals/due-deligence-visuals.component.ts
+++ src/app/pages/evaluation-category-page/due-deligence-report/due-deligence-visuals/due-deligence-visuals.component.ts
@@ -85,64 +85,7 @@ export class DueDeligenceVisualsComponent implements OnInit {
     //this.getSummaryStats('evaluation-summary',this.financialYears[0],'')
     this.getSummaryStats('bids-summary',this.financialYears[0],'')
     this.getVisualisation('bids-by-provider',this.financialYears[0],'')
-    this.chartOptions = {
-      series: [
-        {
-          name: "Contract Value",
-          type: "column",
-          data: [44000000, 50545555, 41454545, 67145454, 22745454, 4135422, 20122434, 35223434, 75223434, 32034242, 25723434, 16023434]
-        },
-        {
-          name: "Number of Bids",
-          type: "line",
-          data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
-        }
-      ],
-      chart: {
-        height: 350,
-        type: "line"
-      },
-      stroke: {
-        width: [0, 4]
-      },
-      title: {
-        text: "Bids and Bid Value "
-      },
-      dataLabels: {
-        enabled: true,
-        enabledOnSeries: [1]
-      },
-      labels: [
-        "01 Jan 2001",
-        "02 Jan 2001",
-        "03 Jan 2001",
-        "04 Jan 2001",
-        "05 Jan 2001",
-        "06 Jan 2001",
-        "07 Jan 2001",
-        "08 Jan 2001",
-        "09 Jan 2001",
-        "10 Jan 2001",
-        "11 Jan 2001",
-        "12 Jan 2001"
-      ],
-      xaxis: {
-        type: "datetime"
-      },
-      yaxis: [
-        {
-          title: {
-            text: "Contract Value"
-          }
-        },
-        {
-          opposite: true,
-          title: {
-            text: "Number of Bids"
-          }
-        }
-      ]
-    };  
+    
 
   }
 
@@ -200,25 +143,29 @@ export class DueDeligenceVisualsComponent implements OnInit {
 
     console.log(reportName)
 
-    this._dueDeligenceReportService.getEvaluationBidsByProvider(reportName,financialYear,procuringEntity).subscribe(
+    this._dueDeligenceReportService.getEvaluationBidsByFinancialYear(reportName,financialYear,procuringEntity).subscribe(
       (response )=>{ 
         let data = response.data
         let  x = []
         let  y = []
 
-        console.log(data)
-        data.forEach(element => {
-          if (element.financial_year == financialYear)
-          {
-            x.push(element?.number_of_plans)
-            var e = element?.estimated_amount.split(',')
-            y.push(parseInt(e.join('')))
-          }
-        });
+        let categories=[]
+        let categorieValues=[]
+        let numOfBids=[]
+
+        console.log("BIDS",data)
+        // data.forEach(element => {
+        //   if (element.financial_year == financialYear)
+        //   {
+        //     x.push(element?.number_of_plans)
+        //     var e = element?.estimated_amount.split(',')
+        //     y.push(parseInt(e.join('')))
+        //   }
+        // });
 
          this.topTenHighestContracts = data.sort(function(a, b) {
-          var nameA = a?.estimated_amount.split(',') 
-          var nameB = b?.estimated_amount.split(',') 
+          var nameA = a?.total_estimated_value.split(',') 
+          var nameB = b?.total_estimated_value.split(',') 
           var valueA = parseInt(nameA.join(''))
           var valueB = parseInt(nameB.join(''))
           
@@ -235,58 +182,48 @@ export class DueDeligenceVisualsComponent implements OnInit {
         console.log(x)
         console.log(y)
 
-        let categories=[]
-        let categorieValues=[]
+       
 
 
-        this.topTenHighestContracts.slice(0,10).forEach(element => {
+        this.topTenHighestContracts.forEach(element => {
+          if(element?.provider =='none') return             
 
-          var valueC = element?.estimated_amount.split(',')
+          var valueC = element?.total_estimated_value.split(',')
           var valueD = parseInt(valueC.join(''))
-          categories.push(element.pde_name)
+          categories.push(element.provider)
           categorieValues.push(valueD)
+          numOfBids.push(parseInt(element?.number_of_bids_submitted))
         });
 
 
-        this.chartOptionsEducationStatus = {
+        this.chartOptions = {
           series: [
             {
-              name: "Planned Contract Value",
-              data: categorieValues,
-              fontSize: "12px"
+              name: "Contract Value",
+              type: "column",
+              data: categorieValues
+            },
+            {
+              name: "Number of Bids",
+              type: "line",
+              data: numOfBids
             }
           ],
           chart: {
             height: 350,
-            type: "bar",
-            events: {
-              click: function(chart, w, e) {
-                // console.log(chart, w, e)
-              }
-            }
+            type: "line"
           },
-          colors: [
-            "#008FFB"          
-          ],
-          plotOptions: {
-            bar: {
-              columnWidth: "35%",
-              distributed: false,
-              horizontal:true
-            }
+          stroke: {
+            width: [0, 4]
+          },
+          title: {
+            text: "Bids and Bid Value "
           },
           dataLabels: {
             enabled: true,
-            formatter: function(val) {
-              return NumberSuffix(val,2) 
+            enabledOnSeries: [1]
           },
-          },
-          legend: {
-            show: false
-          },
-          grid: {
-            show: true
-          },
+          
           xaxis: {
             categories: categories,
             labels: {
@@ -296,13 +233,43 @@ export class DueDeligenceVisualsComponent implements OnInit {
                   "#D10CE8",
                 ],
                 fontSize: "12px"
+              },
+              formatter: function(val) {
+                return NumberSuffix(val,2)}
+            }            
+          },
+          yaxis: [
+            {
+              title: {
+                text: "Contract Value"
+              },
+              labels: {
+                style: {
+                  colors: [
+                    "#008FFB",
+                    "#D10CE8",
+                  ],
+                  fontSize: "12px"
+                },
+                formatter: function(val) {
+                  return NumberSuffix(val,2)}
+              }               
+            },
+            {
+              opposite: true,
+              title: {
+                text: "Number of Bids"
               }
             }
-          },
-          title: {
-            text: "Procuring Entity with Top 10 Jighest Value of Planned contracts" +' '+ `(${financialYear})`
-          },
-        };
+          ],
+          // tooltip: {
+          //   y: {
+          //     formatter: function(val) {
+          //       return "UGX " + NumberSuffix(val,2) ;
+          //     }
+          //   }
+          // }
+        };  
           this.isLoading = false
         },
       (error) => {
