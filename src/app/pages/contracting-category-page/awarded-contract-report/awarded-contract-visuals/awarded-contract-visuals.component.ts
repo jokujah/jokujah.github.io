2492306f63da426104754516e275c9d6583a718c
okuja <ajokuja@gmail.com>
2022-05-11 13:08:33 +0300

    Okuja | W.I.P : Adding more visuals for Awarded Contracts

--- src/app/pages/contracting-category-page/awarded-contract-report/awarded-contract-visuals/awarded-contract-visuals.component.ts
+++ src/app/pages/contracting-category-page/awarded-contract-report/awarded-contract-visuals/awarded-contract-visuals.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit , ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ApexAxisChartSeries, ApexChart, ApexDataLabels, ApexFill, ApexLegend, ApexPlotOptions, ApexStroke, ApexTitleSubtitle, ApexTooltip, ApexXAxis, ApexYAxis, ChartComponent } from 'ng-apexcharts';
+import { ApexAxisChartSeries, ApexChart, ApexDataLabels, ApexFill, ApexLegend, ApexNoData, ApexPlotOptions, ApexStroke, ApexTitleSubtitle, ApexTooltip, ApexXAxis, ApexYAxis, ChartComponent } from 'ng-apexcharts';
 import { AwardedContractReportService } from 'src/app/services/ContractCategory/awarded-contract-report.service';
 import { DueDeligenceReportService } from 'src/app/services/EvaluationCategory/due-deligence-report.service';
-import { getFinancialYears, getsortedPDEList, NumberSuffix, sanitizeCurrencyToString } from 'src/app/utils/helpers';
+import { capitalizeFirstLetter, getFinancialYears, getsortedPDEList, NumberSuffix, sanitizeCurrencyToString } from 'src/app/utils/helpers';
 import ROP from './../../../../../assets/ROP.json'
+import { Toast, ToastrService } from 'ngx-toastr';
 
 
 export type ChartOptions = {
@@ -18,6 +19,7 @@ export type ChartOptions = {
   dataLabels: any; // ApexDataLabels;
   fill: ApexFill;
   tooltip: ApexTooltip;
+  noData:ApexNoData;
 };
 
 @Component({
@@ -29,6 +31,12 @@ export class AwardedContractVisualsComponent implements OnInit {
 
   @ViewChild("chart") chart: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
+
+  @ViewChild("chartProcurementMethod") chartProcurementMethod: ChartComponent;
+  public chartOptionsProcurementMethod: Partial<ChartOptions>;
+
+  @ViewChild("chartProcurementType") chartProcurementType: ChartComponent;
+  public chartOptionsProcurementType: Partial<ChartOptions>;
 
   isLoading:boolean = false 
   valueOfContracts;
@@ -48,17 +56,219 @@ export class AwardedContractVisualsComponent implements OnInit {
 
   constructor(
     fb: FormBuilder,
-    private _service: AwardedContractReportService) {
+    private toastr: ToastrService,
+    private _service: AwardedContractReportService
+    ) {
     this.options = fb.group({
       financialYear: this.financialYearControl,
       pde:this.pdeControl
-    });    
+    }); 
+    
+    
+    
   }
 
   ngOnInit(): void {
-    this.getSummaryStats('contracts-summary',this.financialYears[0],'')
-    this.getVisualisation('contracts-by-contract-type',this.financialYears[0],'')
-    
+    this.chartOptions = {
+      series: [
+        {
+          name: "Contract Award Value",
+          type: "column",
+          data: []
+        },
+        {
+          name: "Number of Bids",
+          type: "line",
+          data: []
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line"
+      },
+      stroke: {
+        width: [0, 4]
+      },
+      title: {
+        text: "Awarded Contract Type "
+      },
+      dataLabels: {
+        enabled: true,
+        enabledOnSeries: [1]
+      },
+
+      xaxis: {
+        categories: [],
+        labels: {
+          style: {
+            fontSize: "12px"
+          }
+        }
+      },
+      yaxis: [
+        {
+          title: {
+            text: "Contract Value"
+          },
+          labels: {
+            style: {
+              colors: [
+                "#008FFB",
+              ],
+              fontSize: "12px"
+            },
+            formatter: function (val) {
+              return NumberSuffix(val, 2)
+            }
+          }
+        },
+        {
+          opposite: true,
+          title: {
+            text: "Number of Contracts"
+          }
+        }
+      ],
+      noData: {
+        text: 'Loading Data...'
+      }
+    };
+
+    this.chartOptionsProcurementMethod = {
+      series: [
+        {
+          name: "Contract Award Value",
+          type: "column",
+          data: []
+        },
+        {
+          name: "Number of Bids",
+          type: "line",
+          data: []
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line"
+      },
+      stroke: {
+        width: [0, 4]
+      },
+      title: {
+        text: "Awarded Contract Method"
+      },
+      dataLabels: {
+        enabled: true,
+        enabledOnSeries: [1]
+      },
+
+      xaxis: {
+        categories: [],
+        labels: {
+          style: {
+            fontSize: "12px"
+          }
+        }
+      },
+      yaxis: [
+        {
+          title: {
+            text: "Contract Value"
+          },
+          labels: {
+            style: {
+              colors: [
+                "#008FFB",
+              ],
+              fontSize: "12px"
+            },
+            formatter: function (val) {
+              return NumberSuffix(val, 2)
+            }
+          }
+        },
+        {
+          opposite: true,
+          title: {
+            text: "Number of Contracts"
+          }
+        }
+      ],
+      noData: {
+        text: 'Loading Data...'
+      }
+    };
+
+    this.chartOptionsProcurementType = {
+      series: [
+        {
+          name: "Contract Award Value",
+          type: "column",
+          data: []
+        },
+        {
+          name: "Number of Contracts",
+          type: "line",
+          data: []
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line"
+      },
+      stroke: {
+        width: [0, 4]
+      },
+      title: {
+        text: "Awarded Contract Procurement Type"
+      },
+      dataLabels: {
+        enabled: true,
+        enabledOnSeries: [1]
+      },
+
+      xaxis: {
+        categories: [],
+        labels: {
+          style: {
+            fontSize: "12px"
+          }
+        }
+      },
+      yaxis: [
+        {
+          title: {
+            text: "Contract Value"
+          },
+          labels: {
+            style: {
+              colors: [
+                "#008FFB",
+              ],
+              fontSize: "12px"
+            },
+            formatter: function (val) {
+              return NumberSuffix(val, 2)
+            }
+          }
+        },
+        {
+          opposite: true,
+          title: {
+            text: "Number of Contracts"
+          }
+        }
+      ],
+      noData: {
+        text: 'Loading Data...'
+      }
+    };
+
+    this.getSummaryStats('contracts-summary', this.financialYears[0], '')
+    this.getVisualisation('contracts-by-contract-type', this.financialYears[0], '')
+    this.getVisualisation('contracts-by-procurement-method', this.financialYears[0], '')
+    this.getVisualisation('contracts-by-procurement-type', this.financialYears[0], '')
+
   }
 
   submit(form: FormGroup) {
@@ -101,11 +311,11 @@ export class AwardedContractVisualsComponent implements OnInit {
         this.isLoading = false
         },
       (error) => {
-        // this.isLoading = false;
-        // this.toastr.error("Something Went Wrong", '', {
-        //   progressBar: true,
-        //   positionClass: 'toast-top-right'
-        // });
+        this.isLoading = false;
+        this.toastr.error("Something Went Wrong", '', {
+          progressBar: true,
+          positionClass: 'toast-top-right'
+        });
         this.isLoading = false
         console.log(error)
       }
@@ -118,140 +328,254 @@ export class AwardedContractVisualsComponent implements OnInit {
     this.numberOfContracts = 0
     this.yearOfBids = 0
 
+    this.chart?.updateOptions({
+
+      series: [],
+
+      xaxis: {
+        categories:[],
+        labels: {
+          style: {
+            colors: [
+              "#008FFB",
+              "#D10CE8",
+            ],
+            fontSize: "12px"
+          },
+          formatter: function(val) {
+            return NumberSuffix(val,2)}
+        }            
+      },
+    })
+
+    this.chartProcurementMethod?.updateOptions({
+
+      series: [],
+
+      xaxis: {
+        categories:[],
+        labels: {
+          style: {
+            fontSize: "12px"
+          },
+          formatter: function(val) {
+            return NumberSuffix(val,2)}
+        }            
+      },
+    })
+
+    this.chartProcurementType?.updateOptions({
+
+      series: [],
+
+      xaxis: {
+        categories:[],
+        labels: {
+          style: {
+            fontSize: "12px"
+          },
+          formatter: function(val) {
+            return NumberSuffix(val,2)}
+        }            
+      },
+    })
+
     console.log(reportName)
 
     this._service.getSummaryStatsNofilter(reportName,financialYear,procuringEntity).subscribe(
       (response )=>{ 
         let data = response.data
-        let  x = []
-        let  y = []
+        let categories = []
+        let categorieValues = []
+        let numOfBids = []
+        let sortedData = []
 
-        let categories=[]
-        let categorieValues=[]
-        let numOfBids=[]
+        switch (reportName) {
+          case 'contracts-by-contract-type':           
+            console.log("AWARDED", data)
 
-        console.log("AWARDED",data)
-        // data.forEach(element => {
-        //   if (element.financial_year == financialYear)
-        //   {
-        //     x.push(element?.number_of_plans)
-        //     var e = element?.estimated_amount.split(',')
-        //     y.push(parseInt(e.join('')))
-        //   }
-        // });
+            sortedData = data.sort(function (a, b) {
+              var nameA = a?.contract_value.split(',')
+              var nameB = b?.contract_value.split(',')
+              var valueA = parseInt(nameA.join(''))
+              var valueB = parseInt(nameB.join(''))
 
-         this.topTenHighestContracts = data.sort(function(a, b) {
-          var nameA = a?.contract_value.split(',') 
-          var nameB = b?.contract_value.split(',') 
-          var valueA = parseInt(nameA.join(''))
-          var valueB = parseInt(nameB.join(''))
-          
-          if (valueA >  valueB) {
-            return -1;
-          }
-          if (valueA < valueB) {
-            return 1;
-          }
-          return 0;
-        })
-
-        console.log(this.topTenHighestContracts)
-        console.log(x)
-        console.log(y)
-
-       
-
-
-        this.topTenHighestContracts.forEach(element => {   
-
-          var valueC = element?.contract_value.split(',')
-          var valueD = parseInt(valueC.join(''))
-          categories.push(element.contract_type)
-          categorieValues.push(valueD)
-          numOfBids.push(parseInt(element?.number_of_contracts))
-        });
-
-
-        this.chartOptions = {
-          series: [
-            {
-              name: "Contract Award Value",
-              type: "column",
-              data: categorieValues
-            },
-            {
-              name: "Number of Bids",
-              type: "line",
-              data: numOfBids
-            }
-          ],
-          chart: {
-            height: 350,
-            type: "line"
-          },
-          stroke: {
-            width: [0, 4]
-          },
-          title: {
-            text: "Awarded Contract Type By Number of Bids and Value"
-          },
-          dataLabels: {
-            enabled: true,
-            enabledOnSeries: [1]
-          },
-          
-          xaxis: {
-            categories: categories,
-            labels: {
-              style: {                
-                fontSize: "12px"
+              if (valueA > valueB) {
+                return -1;
               }
-            }            
-          },
-          yaxis: [
-            {
-              title: {
-                text: "Contract Value"
-              },
-              labels: {
-                style: {
-                  colors: [
-                    "#008FFB",
-                  ],
-                  fontSize: "12px"
+              if (valueA < valueB) {
+                return 1;
+              }
+              return 0;
+            })
+            
+            sortedData.forEach(element => {
+              var valueC = element?.contract_value.split(',')
+              var valueD = parseInt(valueC.join(''))
+              categories.push(capitalizeFirstLetter(element.contract_type))
+              categorieValues.push(valueD)
+              numOfBids.push(parseInt(element?.number_of_contracts))
+            });
+            this.chart?.updateOptions({
+              series: [
+                {
+                  name: "Contract Award Value",
+                  type: "column",
+                  data: categorieValues
                 },
-                formatter: function(val) {
-                  return NumberSuffix(val,2)}
-              }               
-            },
-            {
-              opposite: true,
-              title: {
-                text: "Number of Contracts"
+                {
+                  name: "Number of Bids",
+                  type: "line",
+                  data: numOfBids
+                }
+              ],
+              xaxis: {
+                categories: categories,
+                labels: {
+                  style: {
+                    fontSize: "12px"
+                  },
+                  formatter: function (val) {
+                    return NumberSuffix(val, 2)
+                  }
+                }
+              },
+            })
+
+            break;
+          case 'contracts-by-procurement-method':           
+           
+            console.log("Procurement Method", data)
+
+            sortedData = data.sort(function (a, b) {
+              var nameA = a?.value_of_contracts.split(',')
+              var nameB = b?.value_of_contracts.split(',')
+              var valueA = parseInt(nameA.join(''))
+              var valueB = parseInt(nameB.join(''))
+
+              if (valueA > valueB) {
+                return -1;
               }
-            }
-          ],
-          // tooltip: {
-          //   y: {
-          //     formatter: function(val) {
-          //       return "UGX " + NumberSuffix(val,2) ;
-          //     }
-          //   }
-          // }
-        };  
+              if (valueA < valueB) {
+                return 1;
+              }
+              return 0;
+            })
+            
+            sortedData.forEach(element => {
+              var valueC = element?.value_of_contracts.split(',')
+              var valueD = parseInt(valueC.join(''))
+              categories.push(capitalizeFirstLetter(element.procurement_method))
+              categorieValues.push(valueD)
+              numOfBids.push(parseInt(element?.number_of_contracts))
+            });
+
+            this.chartProcurementMethod?.updateOptions({
+              series: [
+                {
+                  name: "Contract Award Value",
+                  type: "column",
+                  data: categorieValues
+                },
+                {
+                  name: "Number of Contracts",
+                  type: "line",
+                  data: numOfBids
+                }
+              ],
+              xaxis: {
+                categories: categories,
+                labels: {
+                  style: {
+                    colors: [
+                      "#008FFB",
+                      "#D10CE8",
+                    ],
+                    fontSize: "12px"
+                  },
+                  formatter: function (val) {
+                    return NumberSuffix(val, 2)
+                  }
+                }
+              },
+            })
+            break;
+          case 'contracts-by-procurement-type':           
+           
+            console.log("Procurement Type", data)
+
+            sortedData = data.sort(function (a, b) {
+              var nameA = a?.contract_value.split(',')
+              var nameB = b?.contract_value.split(',')
+              var valueA = parseInt(nameA.join(''))
+              var valueB = parseInt(nameB.join(''))
+
+              if (valueA > valueB) {
+                return -1;
+              }
+              if (valueA < valueB) {
+                return 1;
+              }
+              return 0;
+            })
+            
+            sortedData.forEach(element => {
+              var valueC = element?.contract_value.split(',')
+              var valueD = parseInt(valueC.join(''))
+              categories.push(capitalizeFirstLetter(element.procurement_type))
+              categorieValues.push(valueD)
+              numOfBids.push(parseInt(element?.number_of_contracts))
+            });
+
+            this.chartProcurementType?.updateOptions({
+              series: [
+                {
+                  name: "Contract Award Value",
+                  type: "column",
+                  data: categorieValues
+                },
+                {
+                  name: "Number of Contracts",
+                  type: "line",
+                  data: numOfBids
+                }
+              ],
+              xaxis: {
+                categories: categories,
+                labels: {
+                  style: {
+                    colors: [
+                      "#008FFB",
+                      "#D10CE8",
+                    ],
+                    fontSize: "12px"
+                  },
+                  formatter: function (val) {
+                    return NumberSuffix(val, 2)
+                  }
+                }
+              },
+            })
+            break;
+
+          }
+         
           this.isLoading = false
         },
       (error) => {
-        // this.isLoading = false;
-        // this.toastr.error("Something Went Wrong", '', {
-        //   progressBar: true,
-        //   positionClass: 'toast-top-right'
-        // });
+        this.isLoading = false;
+        this.toastr.error("Something Went Wrong", '', {
+          progressBar: true,
+          positionClass: 'toast-top-right'
+        });
         this.isLoading = false
         console.log(error)
       }
     )
   }
+
+
+ 
 
   getFontSize() {
     return Math.max(10, 12);
