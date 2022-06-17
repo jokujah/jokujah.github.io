904a8e523ab4261adbf103a990df0a8a8a788558
okuja <ajokuja@gmail.com>
2022-06-17 17:16:02 +0300

    Okuja | Chore : Awarded contracts add table

---
+++ src/app/shared/visualization-table/visualization-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visualization-table',
+  templateUrl: './visualization-table.component.html',
+  styleUrls: ['./visualization-table.component.scss']
+})
+export class VisualizationTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
