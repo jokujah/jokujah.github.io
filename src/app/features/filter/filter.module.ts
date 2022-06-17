d2fa328c53732590f81a7554796a3aaa2864fcad
Simon Onen <sonen8524@gmail.com>
2022-06-17 05:36:39 +0300

    Chore: Add the close button for Pde filter and reformat code

--- src/app/features/filter/filter.module.ts
+++ src/app/features/filter/filter.module.ts
@@ -1,26 +1,24 @@
-import { SharedModule } from './../../shared/shared.module';
-import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MAT_AUTOCOMPLETE_DEFAULT_OPTIONS, MatAutocompleteModule } from '@angular/material/autocomplete'
+
 import { CommonModule } from '@angular/common';
 import { FilterComponent } from './filter.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { MatTooltipModule } from '@angular/material/tooltip';
-
-import { MatAutocompleteModule} from '@angular/material/autocomplete'
 import { MatSelectSearchModule } from 'src/app/shared/mat-select-search/mat-select-search.module';
-
-
-
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { NgModule } from '@angular/core';
+import { SharedModule } from './../../shared/shared.module';
 
 @NgModule({
   declarations: [
     FilterComponent
   ],
   imports: [
-    CommonModule,    
+    CommonModule,
     MatFormFieldModule,
     FormsModule,
     MatSelectModule,
@@ -29,10 +27,14 @@ import { MatSelectSearchModule } from 'src/app/shared/mat-select-search/mat-sele
     MatButtonModule,
     MatTooltipModule,
     MatAutocompleteModule,
-    MatSelectSearchModule
+    MatSelectSearchModule,
+    MatIconModule,
   ],
   exports:[
     FilterComponent
+  ],
+  providers: [
+    {provide: MAT_AUTOCOMPLETE_DEFAULT_OPTIONS, useValue: {autoActiveFirstOption: true}}
   ]
 })
 export class FilterModule { }
